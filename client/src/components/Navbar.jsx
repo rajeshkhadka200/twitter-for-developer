0f2e07921e28c8573908440f31505b760884bf05
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2023-02-11 14:02:17 +0545

    added something

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "../css/components/Navbar.module.css";
 
+
 const Navbar = () => {
   return (
     <>
-     Navbar
+      <div className={styles.navbar_con}>
+        <div className={styles.left}>
+          <div className={styles.logo_con}>
+            <span className={styles.logo}></span>DevHub
+          </div>
+        </div>
+        <div className={styles.right}></div>
+      </div>
     </>
   );
 };
