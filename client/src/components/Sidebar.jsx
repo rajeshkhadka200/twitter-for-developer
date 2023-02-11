3bef80dfc8c01eeb76a5907c6cbc17c4bb730563
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2023-02-11 23:27:33 +0545

    sidebar coded

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../css/components/Sidebar.module.css";
-import { IconButton } from "@pankod/refine-mui";
+import { Button, IconButton } from "@pankod/refine-mui";
 import { SiCodemagic } from "react-icons/si";
 import { AiFillHome, AiOutlineHome } from "react-icons/ai";
 import { FiGlobe } from "react-icons/fi";
@@ -8,18 +8,23 @@ import { BsLaptop, BsLaptopFill } from "react-icons/bs";
 import { MdOutlineLibraryBooks, MdLibraryBooks } from "react-icons/md";
 // import { HiUserCircle, HiOutlineUserCircle } from "react-icons/hi";
 import { IoSettingsSharp, IoSettingsOutline } from "react-icons/io5";
+import { NavLink, useLocation } from "@pankod/refine-react-router-v6";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const { pathname } = location;
+
   const links = [
     {
       name: "Home",
-      icon1: <AiFillHome />,
-      icon2: <AiOutlineHome />,
+      icon2: <AiFillHome />,
+      icon1: <AiOutlineHome />,
       link: "/app",
     },
     {
       name: "Discover",
-      icon: <FiGlobe />,
+      icon1: <FiGlobe />,
+      icon2: <FiGlobe />,
       link: "/discover",
     },
     {
@@ -32,19 +37,19 @@ const Sidebar = () => {
       name: "Blogs",
       icon1: <MdOutlineLibraryBooks />,
       icon2: <MdLibraryBooks />,
-      links: "/blogs",
+      link: "/blogs",
     },
-    {
-      name: "Profile",
-      icon1: <HiUserCircle />,
-      icon2: <HiOutlineUserCircle />,
-      links: "/profile",
-    },
+    // {
+    //   name: "Profile",
+    //   icon1: <HiUserCircle />,
+    //   icon2: <HiOutlineUserCircle />,
+    //   links: "/profile",
+    // },
     {
       name: "Api Key",
-      icon1: <IoSettingsSharp />,
-      icon2: <IoSettingsOutline />,
-      links: "/api",
+      icon2: <IoSettingsSharp />,
+      icon1: <IoSettingsOutline />,
+      link: "/api",
     },
   ];
   return (
@@ -54,16 +59,64 @@ const Sidebar = () => {
           <div className={styles.logo}>
             <IconButton
               aria-label="logo"
-              sx={{ width: "50px", height: "50px", fontSize: "30px" }}
+              sx={{
+                width: "50px",
+                height: "50px",
+                fontSize: "30px",
+                marginLeft: "10px",
+                mt: "5px",
+              }}
             >
               <SiCodemagic />
             </IconButton>
           </div>
-          <div className={styles.link}>
-            <div className={styles.link_logo}>
-              <AiFillHome />
+          {links.map((link) => (
+            <NavLink
+              className={
+                pathname === link.link ? styles.link_active : styles.link
+              }
+              to={link.link}
+            >
+              <div className={styles.link_logo}>
+                {pathname === link.link ? link.icon2 : link.icon1}
+              </div>
+              <div
+                className={
+                  pathname === link.link
+                    ? styles.link_name_active
+                    : styles.link_name
+                }
+              >
+                {link.name}
+              </div>
+            </NavLink>
+          ))}
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "100vw",
+              color: "text.normal",
+              fontSize: "1.2rem",
+              fontFamily: "Poppins",
+              textTransform: "none",
+              py: "8px",
+              width: "90%",
+            }}
+          >
+            Devit
+          </Button>
+          <div className={styles.profile_handle}>
+            <div className={styles.profile}>
+              <img
+                src="/pic.jpg"
+                alt="profile"
+                className={styles.profile_img}
+              />
             </div>
-            <div className={styles.link_name}>Home</div>
+            <div className={styles.name_con}>
+                <div className={styles.name}>Utsav Bhattarai</div>
+                <div className={styles.username}>@utsav</div>
+            </div>
           </div>
         </div>
       </div>
