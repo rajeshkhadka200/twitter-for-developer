a2b00095ea9ec27b9998de4eb6abdff548b0c5b7
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-02-15 01:00:27 +0545

    hackathon card frame

--- client/src/pages/Hackathons.jsx
+++ client/src/pages/Hackathons.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import PageTop from '../components/PageTop';
+import React from "react";
+import HackathonCard from "../components/HackathonCard";
+import PageTop from "../components/PageTop";
 
 const Hackathons = () => {
   return (
     <>
       <PageTop label="Hackathons" />
+      <HackathonCard />
     </>
   );
-}
+};
 
-export default Hackathons
+export default Hackathons;
