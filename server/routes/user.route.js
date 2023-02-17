f2ac38463a97c33bcbf22e16ee0931d68d197123
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-02-18 01:57:53 +0545

    bug fix

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,10 +2,14 @@ import express from "express";
 const router = express.Router();
 
 // import controlers
-import { auth, updateProfile,getMyprofile } from "../controllers/user.controllers.js";
+import {
+  auth,
+  updateProfile,
+  getMyprofile,
+} from "../controllers/user.controllers.js";
 
 router.post("/auth", auth); // auth is the route
-router.patch("/:userId", updateProfile); // auth is the route
-router.get("/:userId", getMyprofile); // auth is the route
+router.patch("/:userId", updateProfile); // auth route
+router.get("/:userId", getMyprofile); // get user data
 
 export default router;
