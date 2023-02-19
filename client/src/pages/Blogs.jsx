74f104741bcb7723521b2784f636c5853c8c6e71
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2023-02-19 20:46:02 +0545

    blog and hacks api integarated

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BlogCard from "../components/BlogCard";
 import PageTop from "../components/PageTop";
+import Loader from "../components/Loader";
+import provider from "../config/axios";
+
+
 
 const Blogs = () => {
+   const [blogs, setBlogs] = React.useState([]);
+   const [loading, setLoading] = React.useState(true);
+
+   useEffect(() => {
+     getBlogs();
+   }, []);
+
+   const getBlogs = async () => {
+     try {
+       const res = await provider.get("/bot/blogs/all");
+       if (res) {
+         setBlogs(res.data.blogs);
+         setLoading(false);
+       }
+     } catch (error) {
+       console.log(error);
+       setLoading(false);
+     }
+   };
   return (
     <>
       <PageTop label="Blogs" />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      {/* <BlogCard /> */}
+      {loading ? (
+        <Loader height="80vh" />
+      ) : blogs?.length > 0 ? (
+        blogs.map((blog) => (
+          <BlogCard key={blog._id} blog={blog} />
+        ))
+      ) : (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "80vh",
+            width: "100%",
+            fontSize: "1.2rem",
+            fontWeight: "500",
+            color: "#fff",
+          }}
+        >
+          No Blogs Found
+        </div>
+      )}
     </>
   );
 };
