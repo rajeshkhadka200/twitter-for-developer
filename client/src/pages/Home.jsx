1b33338953c16ce5b30cc90fda71bed0017206af
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-02-19 21:39:21 +0545

    fetching in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Devit from "../components/Devit";
 import PageTop from "../components/PageTop";
 import Post from "../components/Post";
@@ -9,12 +9,17 @@ const Home = () => {
   const { devits } = useContext(ContextProvider);
   const [alldevits, setAllDevits] = devits;
 
+  // randomly sort the devits in array
+  // useEffect(() => {
+  //   alldevits.sort(() => Math.random() - 0.5);
+  // }, []);
+
   return (
     <>
       <PageTop label="Home" />
       <div className={styles.home_container}>
         <Devit />
-        {sortedDevits.map((data, i) => {
+        {alldevits.map((data, i) => {
           return <Post data={data} key={i} />;
         })}
       </div>
