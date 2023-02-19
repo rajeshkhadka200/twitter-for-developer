c06bd8c0df84c2e1452ab5dd7e4ab26680a1e093
rajeshkhadka <rajeshkhadkaofficia@gmail.coml>
2023-02-19 14:00:36 +0545

    post

---
+++ server/controllers/image.controller.js
@@ -0,0 +1,23 @@
+export const postImage = async (req, res) => {
+  try {
+    const { image } = req.files;
+    const filename = Date.now() + image.name.replace(/\s/g, "");
+    const url = `${req.protocol}://${req.get("host")}/uploads/${filename}`;
+
+    await image.mv("uploads/" + filename, function (err) {
+      if (err) {
+        throw err;
+      }
+    });
+
+    return res.status(200).json({
+      url,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      error: error,
+      message: "unable to upload image",
+    });
+  }
+};
